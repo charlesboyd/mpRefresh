7e897b8293e58c481f04d909dc77ea677a6e4b36
Charles Boyd <charles@powerimagemedia.com>
2015-11-30 10:36:58 -0600

    Initial Commit

---
+++ mpRefresh.js
@@ -0,0 +1,13 @@
+/*
+
+    mpRefresh.js
+
+    Copyright (c)2015 Charles Boyd (charlesboyd.me). All rights reserved.
+    
+*/
+
+var mpRefresh = function(){
+
+    console.log("Hello World");
+
+}();
