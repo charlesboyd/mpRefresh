325b5200be8565b0d5e849b26779fce27f282dd2
Charles Boyd <charles@powerimagemedia.com>
2015-11-30 10:39:20 -0600

    Basic Ajax Request

--- mpRefresh.js
+++ mpRefresh.js
@@ -8,6 +8,44 @@
 
 var mpRefresh = function(){
 
+    var refreshIntervalS = 30;
+
     console.log("Hello World");
+
+    var requestPage = function(){
+        var xhttp;
+        if (window.XMLHttpRequest) {
+            xhttp = new XMLHttpRequest();
+        }else{
+            //For IE6 and before
+            xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                responseRecieved(xhttp.responseText);
+            }else if(xhttp.readyState == 4){
+                requestError();
+            }else{}
+        };
+        
+        var pageURL = window.location.href;
+        console.log(pageURL);
+        
+        xhttp.open("GET", pageURL, true);
+        xhttp.send();
+    };
+    
+    var responseRecieved = function(newHTML){
+        console.log(newHTML);
+    }
+    
+    var requestError = function(){
+        console.log("Request Error!");
+    }
+    
+    
+    
+    requestPage();
 
 }();
